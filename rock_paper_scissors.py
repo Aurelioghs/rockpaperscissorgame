c6ca19f200bedefb85f6ffc15c6fd902f0e4d488
Aurelio Lopez <aurelioghs@yahoo.com>
2022-06-29 01:22:18 -0700

    Added winning and losing options

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,17 +1,33 @@
 #computer will choose random number
 import random
 #welcome message
-print("Hi!")
+print("\nHi! Let's play Rock Paper Scissor")
 
 def play():
-#take input from user
-    user_input = input("Select your option [rock, paper, scissors]: ")
-#computer to randomly chose an option
-    computer_option = ["rock", "paper", "scissors"]
-    choose_option = random.choice(computer_option)
-#output selections to the user
-    print("********  You selected: " , user_input)
-    print("********  Compubot:", choose_option)
+    #take input from user
+    user_input = input("Select your option [rock, paper, scissor]: ")
+    #computer to randomly chose an option
+    # computer_option = ["rock", "paper", "scissors"]
+    computer_option = random.choice(["rock", "paper", "scissor"])
+    #output selections to the user
+    print("********  You selected: ", user_input)
+    print("********  Compubot: ", computer_option)
 
-    if user_input == choose_option:
-        return "It's a tie!"
+    if user_input == computer_option:
+        return "********  It's a tie!"
+    #will return the winner
+    if winner(user_input, computer_option):
+        return "********  You won!"
+    #will return the loser
+    return "********  You lost!"
+    #function for the winner will return true
+def winner(user, compubot):
+    #paper>rock
+    #rock>scissor
+    #scissor>paper
+    if(user == 'rock' and compubot == 'scissor') \
+    or (user == 'scissor' and compubot == 'paper') \
+    or (user == 'paper' and compubot == 'rock'):
+        return True
+
+print(play())
